4a58b537d1e432b806317039b6357cd2bb2262d9
Andrew <thebuffylow@gmail.com>
2022-08-16 01:29:12 -0500

    fix

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -9,7 +9,7 @@ import { Satellite } from '../satellite';
 export class OrbitCountsComponent implements OnInit {
 
 	@Input() satellites: Satellite[];
-	categories: string[] = ['Total', 'Space Debris', 'Communication', 'Probe', 'Positioning', 'Space Station', 'Telescope'];
+	
   constructor() { }
 
   ngOnInit() {
